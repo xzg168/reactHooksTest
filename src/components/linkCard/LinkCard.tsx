f44b70117b5eb092f2ab01f4244f6b239750ab6b
xzg168 <741491161@qq.com>
2019-09-23 15:25:26 +0800

    修改

--- src/components/linkCard/LinkCard.tsx
+++ src/components/linkCard/LinkCard.tsx
@@ -1,27 +1,30 @@
 import * as React from 'react';
-import {Badge} from 'antd';
-import styles from './styles.css'
+import styles from './styles.css';
 interface Props {
-  count?:number;
-  icon?:any;
-  title?:string;
-  backgroundColor?:string;
-  onClick?:()=>void;
+  count?: number;
+  icon?: any;
+  title?: string;
+  backgroundColor?: string;
+  onClick?: () => void;
 }
-export default function LinkCard(props:Props){
-    return(
-      <div className={styles.linkCardLayout}
-         style={props.backgroundColor?{backgroundColor:props.backgroundColor}:{}}
-         onClick={props.onClick}
-      >
-         <div className={styles.badgeContent}
-      >
-        {props.count?<Badge count={props.count}/>:''}
+export default function LinkCard(props: Props) {
+  return (
+    <div
+      className={styles.linkCardLayout}
+      style={
+        props.backgroundColor ? { backgroundColor: props.backgroundColor } : {}
+      }
+      onClick={props.onClick}
+    >
+      <div className={styles.badgeContent}>
+        {props.count ? (
+          <span className={styles.infoCardBage}>{props.count}</span>
+        ) : (
+          '1'
+        )}
       </div>
-         <div className={styles.linkCardIcon}>
-              {props.icon}
-         </div>
-         <div className={styles.linkCardTitle}>{props.title}</div>
-      </div>
-    )
+      <div className={styles.linkCardIcon}>{props.icon}</div>
+      <div className={styles.linkCardTitle}>{props.title}</div>
+    </div>
+  );
 }
